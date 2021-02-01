7bd783c448cf15e6224b54f43941f2fab2f04b55
Mendes82 <77982712+Mendes82@users.noreply.github.com>
2021-02-01 01:58:50 +0000

    Add files via upload

---
+++ element_queue.cpp
@@ -0,0 +1,101 @@
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <iostream> 
+#include <unistd.h>
+#include <functional>
+#include <atomic>
+
+template <class T> class Queue
+{
+	private:
+		std::mutex mutex_;
+		bool waitFinished;
+		int max_size;
+		int ReadingSleep;
+        int WritingSleep;
+	public:
+    	std::queue<T> data;
+		Queue(int max_size = 2)
+			: max_size(max_size),waitFinished(false),ReadingSleep(50), WritingSleep(50)
+			{}
+		
+		void push(T element)
+			{
+				while (data.size() >= max_size)
+				{
+				 std::unique_lock<std::mutex> mlock(mutex_);
+				 std::cout << "block, waiting for pop" << std::endl;
+				 mlock.unlock();
+				 std::this_thread::sleep_for(std::chrono::milliseconds(WritingSleep));
+				 waitFinished = true;
+				}
+				std::unique_lock<std::mutex> mlock(mutex_);
+				data.push(element);
+                std::cout << "Write, queue element: " << element << " size: " << data.size() << std::endl;
+				mlock.unlock();
+               
+			}
+		T pop()
+			{
+				while (data.size() <= 0)
+				{
+				 std::unique_lock<std::mutex> mlock(mutex_);
+				 std::cout << "block, waiting for push " << std::endl;
+				 mlock.unlock();
+				 std::this_thread::sleep_for(std::chrono::milliseconds(ReadingSleep));
+				 waitFinished = true;
+				}
+				std::unique_lock<std::mutex> mlock(mutex_);
+				T element = data.front(); 
+				data.pop();
+                std::cout << "Read, front element: " << element << " size: " << data.size() << std::endl;
+				mlock.unlock();
+				return element;
+			}
+		int size() const
+			{
+			 std::unique_lock<std::mutex> mlock(mutex_);
+			 int result = data.size();
+			 mlock.unlock();
+			 return result;
+			}
+};
+
+void Write(Queue<int>& q) 
+{
+  for (int i = 1; i< 100; ++i) 
+  {
+   q.push(i);
+  }
+}
+
+void Read(Queue<int>& q) 
+{
+  for (int i = 1; i< 100; ++i) 
+  {
+    q.pop();
+  }
+}
+
+int main() {
+ Queue<int> q;
+
+  using namespace std::placeholders;
+
+  // Writing thread
+  std::thread Writing(std::bind(&Write, std::ref(q)));
+
+  // Reading thread
+  std::thread Reading(std::bind(&Read, std::ref(q)));
+
+  Writing.join();
+  Reading.join();
+ 
+ std::cout << "test finished, loop exited" << std::endl;
+
+  return 0;
+}
+
+
